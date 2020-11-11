e955430c87437f1cdc0369d58bf756d93f48566d
Caio Victor <caiovictormota@gmail.com>
2020-11-11 14:20:12 -0300

    Building Sessions Endpoint. Fixes on Session Model

--- controllers/session.js
+++ controllers/session.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const Campaign = require('../models/campaign.js')
 const Session = require('../models/session.js')
 const campaignRouter = require('./campaign.js')
 const checkCampaignExistence = require('../middlewares/checkCampaignExistence')
@@ -14,17 +15,57 @@ router.use(endPoint, checkCampaignExistence)
 //All Sessions Routes
 router.route(endPoint)
 .post(isCampaignActive, async (req, res) => {
-    res.send('Post Ok. CID: ' + req.params[campaignRouter.idParam])
+    try{
+        let campaign = await Campaign.findById(
+            req.params[campaignRouter.idParam])
+
+        const newSession = new Session(req.body)
+        campaign.sessions.push(newSession)
+
+        campaign = await Campaign.findByIdAndUpdate(
+            req.params[campaignRouter.idParam], campaign)
+
+        const location = (endPoint + newSession._id).replace(
+            ':'+campaignRouter.idParam, req.params[campaignRouter.idParam])
+
+        res.status(201)
+            .location(location)
+            .send()
+    } catch (error) {
+        res.status(400).send(error)
+    }
 })
 .get(async (req, res) => {
-    res.send('Get All OK. CID: ' + req.params[campaignRouter.idParam])
+    try {
+        const campaign = await Campaign.findById(
+            req.params[campaignRouter.idParam])
+
+        if (campaign.sessions.length !== 0) {
+            return res.status(200).send(campaign.sessions)
+        }
+        res.status(204).send()
+    } catch (error) {
+        res.status(500).send(error)
+    }
 })
 
 //Specific Sessions Routes
 router.route(endPoint + ':' + idParam)
 .get(async (req, res) => {
-    res.send('Get One OK. CID: ' + req.params[campaignRouter.idParam] 
-    + '; SID: ' + req.params[idParam])
+    try {
+        const campaign = await Campaign.findById(
+            req.params[campaignRouter.idParam])
+    
+        const session = campaign.sessions.find(
+            session => session._id == req.params[idParam])
+
+        if (session) {
+            return res.status(200).send(session)
+        }
+        res.status(204).send()
+    } catch (error) {
+        res.status(500).send(error)
+    }
 })
 .put(async (req, res) => {
     res.send('PUT One OK. CID: ' + req.params[campaignRouter.idParam] 
--- models/session.js
+++ models/session.js
@@ -3,12 +3,7 @@ const mongoose = require('mongoose')
 const sessionSchema = new mongoose.Schema({
     startingDateTime: {type: Date, required: true},
     endingDateTime: {type: Date, required: true},
-    notes: [{
-        note: {
-            type: String,
-            trim: true
-        }
-    }]
+    notes: [{type: String, trim: true}]
 }, {
     timestamps: true
 })
