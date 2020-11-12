ab83aa13938af92357481c74f9f3bad8271e85fb
Caio Victor <caiovictormota@gmail.com>
2020-11-12 14:26:19 -0300

    Fixing Environment Variables

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,11 +1,7 @@
 //require('dotenv').config()
 const mongoose = require('mongoose')
 
-const DB_HOST = process.env.DB_HOST
-const DB_PORT = process.env.DB_PORT
-const DB_NAME = process.env.DB_NAME
-
-const connectionUrl = DB_HOST + DB_PORT + DB_NAME
+const connectionUrl = process.env.DB_URL
 
 mongoose.connect(connectionUrl, {
     useNewUrlParser: true,
