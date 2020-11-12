85ebac0f94d4b72d079f01c8d71b0ec0172bebf6
Caio Victor <caiovictormota@gmail.com>
2020-11-12 13:46:51 -0300

    Some fixes on connection to database

--- db/mongoose.js
+++ db/mongoose.js
@@ -5,7 +5,7 @@ const DB_HOST = process.env.DB_HOST
 const DB_PORT = process.env.DB_PORT
 const DB_NAME = process.env.DB_NAME
 
-const connectionUrl = 'mongodb://' + DB_HOST + ':' + DB_PORT + '/' + DB_NAME
+const connectionUrl = DB_HOST + DB_PORT + DB_NAME
 
 mongoose.connect(connectionUrl, {
     useNewUrlParser: true,
